4fb96cb590424daad5c6e3ae60e326649afe8b2e
Carlos León <carlosestebanleonpinilla@gmail.com>
2021-03-07 20:11:56 -0500

    app ok

--- src/js/app.js
+++ src/js/app.js
@@ -12,18 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
             container.append(wrapperNodes)
         }
-
-
-
-
-
-
-
-
-
-
-
-
+        // Fetchin data 
         fetch('https://hp-api.herokuapp.com/api/characters')
             .then(response => response.json())
             .then(data => createCards(data))
